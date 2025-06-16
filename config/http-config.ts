8cecc9cace24a5784e59b29f850816141baa4ea0
Yogi Prasetya <hello@yogiprasetya.com>
2025-06-16 20:11:53 +0700

    n8n auth

--- config/http-config.ts
+++ config/http-config.ts
@@ -4,7 +4,7 @@ export const apiClient = axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com",
   timeout: 100000,
   headers: {
-    Authorization: "Basic dGVzdGluZy1hZG1pbjp0ZXN0aW5nYWRtaW4=",
+    Authorization: `Basic ${process.env.NEXT_PUBLIC_AUTH_N8N}`,
     "X-Requested-With": "XMLHttpRequest",
   },
 });
